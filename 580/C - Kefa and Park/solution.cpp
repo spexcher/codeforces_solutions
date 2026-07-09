ecb8abe86683e617c8912a97a210d9f11281ec84
Gourab Modak <137685981+spexcher@users.noreply.github.com>
2026-07-09 07:29:59 +0530

    Add Kefa and Park [C] from Codeforces

---
+++ 580/C - Kefa and Park/solution.cpp
@@ -0,0 +1,208 @@
+// ------------------------------------------------------------------------------------
+// Problem Name: C. Kefa and Park
+// From Contest: Codeforces - Codeforces Round 321 (Div. 2)
+// URL: https://codeforces.com/contest/580/problem/C
+// Memory Limit: 256 MB (Mega Bytes)
+// Time Limit: 2000 ms (milliseconds)
+//
+// Author: spexcher
+// Name: Gourab Modak
+// email: spexcher@gmail.com
+// Institution: Indian Institute of Technology Madras(BS UG Data Science 4 year)
+// + Jalpaiguri Government Engineering College (B. Tech UG CSE 4 year)
+//
+// Linktree: https://linktr.ee/spexcher/
+// Github: https://github.com/spexcher/
+// linkedin : https://www.linkedin.com/in/gourabmodak/
+// ------------------------------------------------------------------------------------
+ 
+#include <bits/stdc++.h>
+using namespace std;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T>
+using pbds =
+    tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+ 
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+template <typename T>
+using pair2 = pair<T, T>;
+using pii = pair<int, int>;
+using pli = pair<ll, int>;
+using pll = pair<ll, ll>;
+#define ll long long
+#define ull unsigned long long
+#define lld long double
+#define mod MOD
+#define eb emplace_back
+#define pb push_back
+#define ppb pop_back
+#define sqrt(x) sqrtl((x))
+#define ceil(x) ceill((x))
+#define floor(x) floorl((x))
+#define ceildiv(a, b) ((a + b - 1) / b)
+#define vi vector<int>
+#define vll vector<ll>
+#define vvb vector<vector<bool>>
+#define vvc vector<vector<char>>
+#define vb vector<bool>
+#define vc vector<char>
+#define vs vector<string>
+#define vvi vector<vector<int>>
+#define vvll vector<vector<ll>>
+#define vvb vector<vector<bool>>
+#define vvpii vector<vector<pii>>
+#define vvpll vector<vector<pll>>
+#define vpii vector<pii>
+#define vpll vector<pll>
+#define si set<int>
+#define sll set<ll>
+#define sc set<char>
+#define mii map<int, int>
+#define mll map<ll, ll>
+#define mci map<char, int>
+#define mcll map<char, ll>
+#define pqi priority_queue<int>
+#define pqll priority_queue<ll>
+ 
+#define lb lower_bound
+#define ub upper_bound
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sortall(x) sort(all(x))
+#define ff first
+#define ss second
+#define spre(x) cout << fixed << setprecision(x)
+#define PI 3.1415926535897932384626
+#define py cout << "YES
+"
+#define pm cout << "-1
+"
+#define pn cout << "NO
+"
+#define pm cout << "-1
+"
+#define pl cout << "
+"
+#define nl "
+"
+#define sp " "
+#define print(x) cout << x << "
+"
+#define deb(x) cout << #x << "=" << x << endl
+#define upper(s1) transform(s1.begin(), s1.end(), s1.begin(), ::toupper)
+#define lower(s1) transform(s1.begin(), s1.end(), s1.begin(), ::tolower)
+#define maxe(v) *max_element(v.begin(), v.end())
+#define mine(v) *min_element(v.begin(), v.end())
+#define unq(v) v.resize(distance(v.begin(), unique(all(v))))
+#define sum(v) accumulate(all(v), 0)
+#define sz(x) (ll) x.size()
+#define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
+#define gc getchar_unlocked
+#define fo(i, n) for (int i = 0; i < n; i++)
+#define forr(i, n) for (int i = n - 1; i >= 0; i--)
+#define Fo(i, k, n) \
+  for (int i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define setbits(n) __builtin_popcountll(n)
+#define counttz(n) __builtin_ctzll(n)
+#define countlz(n) __builtin_clzll(n)
+clock_t startTime;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+//------------take ip/op like vector,pairs directly!-------------------//
+template <typename typC, typename typD>
+istream &operator>>(istream &cin, pair<typC, typD> &a) {
+  return cin >> a.first >> a.second;
+}
+template <typename typC>
+istream &operator>>(istream &cin, vector<typC> &a) {
+  for (auto &x : a) cin >> x;
+  return cin;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const pair<typC, typD> &a) {
+  return cout << a.first << ' ' << a.second;
+}
+template <typename typC, typename typD>
+ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a) {
+  for (auto &x : a) cout << x << '
+';
+  return cout;
+}
+template <typename typC>
+ostream &operator<<(ostream &cout, const vector<typC> &a) {
+  int n = a.size();
+  if (!n) return cout;
+  cout << a[0];
+  for (int i = 1; i < n; i++) cout << ' ' << a[i];
+  return cout;
+}
+//----------------------END Of the input module --------------------
+#define int long long
+void solve();
+signed main() {
+  startTime = clock();
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  int t = 1;
+  // cin >> t;
+  for (int i = 1; i <= t; i++) {
+    // eprintf("--- Case #%lld start ---
+", i);
+    // eprintf("Case #%lld: ", i);
+    // solve();
+    // eprintf("--- Case #%lld end ---
+", i);
+    // eprintf("time = %.5lf
+", getCurrentTime());
+    // eprintf("++++++++++++++++++++
+");
+ 
+    solve();
+  }
+ 
+  return 0;
+}
+//-----------------------------End Snippet--------------------------
+ 
+const int N = 2e5 + 50;
+vvi g(N);
+vi cat(N);
+int ans = 0;
+int n, m;
+void dfs(int node, int parent, int maxcat, int c_cat) {
+  if (cat[node] == 0)
+    c_cat = 0;
+  else
+    c_cat++;
+  maxcat = max(maxcat, c_cat);
+  // traverse all its neighbours
+  int nchild = 0;
+  for (auto it : g[node]) {
+    // if the neighbour is not visited
+    if (it != parent) {
+      nchild++;
+      dfs(it, node, maxcat, c_cat);
+    }
+  }
+  if (nchild == 0 and maxcat <= m) ans++;
+}
+ 
+void solve() {
+  cin >> n >> m;
+  Fo(i, 1, n + 1) cin >> cat[i];
+  int u, v;
+  Fo(i, 1, n) {
+    cin >> u >> v;
+    g[v].pb(u);
+    g[u].pb(v);
+  }
+  dfs(1ll, -1ll, 0ll, 0ll);
+  print(ans);
+}
